f314c2bd0dc97fa97ccba30c5a9176962dbb0c45
grizz <grizz@AndrewPC.Home>
2020-05-18 10:26:11 -0400

    Initial Commit

---
+++ Integration Project/src/IntegrationProject.java
@@ -0,0 +1,17 @@
+//Andrew Griswold
+//This program will keep track of inventory sold over different e-commerce sites
+
+public class IntegrationProject {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		
+		boolean addInventory;
+		int itemOne;
+		double costOfGoodsSold;
+		
+		final double taxCost;
+	}
+}
+
+
