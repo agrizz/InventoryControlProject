03f2d4ce1a31b8940e69dc211085e1e87e655acd
grizz <grizz@AndrewPC.Home>
2020-06-01 21:52:08 -0400

    loop added

--- Integration Project/src/IntegrationProject.java
+++ Integration Project/src/IntegrationProject.java
@@ -15,56 +15,84 @@
 
 
 import java.util.Scanner; // scanner java utility
+import java.util.Random;
 
 public class IntegrationProject {
 
   public static void main(String[] args) {
 
     try (Scanner scan = new Scanner(System.in)) {
-    int itemOne = 25; // # of inventory of itemOne
-    double inventoryValue = 0; // $ value of inventory
-    final double PRODUCTCOST = 24.05; // putting final in front of a variable means it cannot be                                      
-    String prompt = "Are you adding (true) or subtracting (false) inventory?";        // changed
-    String correctPassword = "CTG";
+      int itemOne = 25; // # of inventory of itemOne
+      double inventoryValue = 0; // $ value of inventory
+      Random rand = new Random();
+      final double PRODUCTCOST = 24.05; // putting final in front of a variable means it cannot be
+      String prompt = "Are you adding (true) or subtracting (false) inventory?"; // changed
+      String correctPassword = "CTG";
 
-    System.out.println("Welcome");
-    System.out.println("What is the password?");
-    String inputPassword = scan.nextLine();
-    inputPassword = inputPassword.trim();
-    inputPassword = inputPassword.toUpperCase();
-    
-    if (inputPassword.equals(correctPassword)) {
-      
-      System.out.println("Access Granted");
-      System.out.print("Your current inventory is ");
-      System.out.println(itemOne);
-      System.out.println(prompt);
-  
+      System.out.println("Welcome");
+      System.out.println("What is the password?");
+
+      String inputPassword = scan.nextLine();
+      inputPassword = inputPassword.trim();
+      inputPassword = inputPassword.toUpperCase();
+
+      if (inputPassword.equals(correctPassword)) {
+
+        System.out.println("Are you a robot?");
+        System.out.println("Repeat back the number shown below.");
+        int timesRun = 0;
+
+        while (timesRun < 3) {
+
+          int randomInt = rand.nextInt(100);
+          System.out.println(randomInt);
+          int inputValue = scan.nextInt();
+
+          if (randomInt == inputValue) {
+            break; // breaks the loop and continues the program
+          } else
+            timesRun++;
+
+          if (timesRun == 3) {
+            System.out.println("Access Denied");
+            System.exit(timesRun);
+          } else {
+            continue; // continues running the loop
+
+          }
+        }
+
+        System.out.println("Access Granted");
+        System.out.print("Your current inventory is ");
+        System.out.println(itemOne);
+        System.out.println(prompt);
+
         boolean addInventory = scan.nextBoolean(); // scans for true or false value
-  
+
         if (addInventory == true) { // runs if true
           System.out.println("How many items would you like to add?");
           itemOne = itemOne + scan.nextInt(); // adds the next int input to the current inventory of
                                               // item one
-          inventoryValue = itemOne * PRODUCTCOST; // multiplies the inventory of item one by the product
+          inventoryValue = itemOne * PRODUCTCOST; // multiplies the inventory of item one by the
+                                                  // product
                                                   // cost
           System.out.print("The available inventory is now ");
           System.out.println(itemOne);
           System.out.print("The current inventory value is $");
           System.out.println(inventoryValue);
-        }
-        else // runs if false
+        } else // runs if false
           System.out.println("How many items would you like to subtract");
-          itemOne = itemOne - scan.nextInt(); // subtracts the next int input from the current inventory
-                                             // of item one   
-          inventoryValue = itemOne * PRODUCTCOST; // multiplies the inventory of item one by the product
-                                              // cost
-          System.out.print("The available inventory is now ");
-          System.out.println(itemOne);
-          System.out.print("The current inventory value is $");
-          System.out.println(inventoryValue);
-      }
-    else
+        itemOne = itemOne - scan.nextInt(); // subtracts the next int input from the current
+                                            // inventory
+                                            // of item one
+        inventoryValue = itemOne * PRODUCTCOST; // multiplies the inventory of item one by the
+                                                // product
+        // cost
+        System.out.print("The available inventory is now ");
+        System.out.println(itemOne);
+        System.out.print("The current inventory value is $");
+        System.out.println(inventoryValue);
+      } else
         System.out.println("Access Denied");
     }
   }
