0594c818ee8ef6728b13e5d255638a004aefaa35
grizz <grizz@AndrewPC.Home>
2020-06-19 19:29:28 -0400

    3

--- Integration Project/src/IntegrationProject.java
+++ Integration Project/src/IntegrationProject.java
@@ -14,14 +14,20 @@
 // modifiers can be used to allow or disallow code to be used across different classes.
 
 // operator precedence is the order that operators are used in when put in a sequence together
-//is the same as order of operations in math : 
-//Parenthesis Exponents Multiplication Division Addition Subtraction
+// is the same as order of operations in math :
+// Parenthesis Exponents Multiplication Division Addition Subtraction
+
+//Polymorphism is the trait that objects have to take multiple forms
+//One object can have multiple different uses in different situations
+//example of this is when overloading a method
+
+//inheritance is a feature in java in which one class can use the methods and fields from another class
 
 
 import java.util.Scanner; // scanner java utility
 import java.util.Random;
 
-public class IntegrationProject { 
+public class IntegrationProject {
 
 
   public static double total(double inventoryValueOne, double inventoryValueTwo,
@@ -39,6 +45,36 @@ public class IntegrationProject {
     int intCasting = 25;
     double doubleCasting = intCasting; // casting changes the data type
 
+    int values[] = new int[3];
+    values[0] = data.inventoryOne;
+    values[1] = data.inventoryTwo;
+    values[2] = data.inventoryThree;
+    double inventorySum = 0;
+    int min = values[0];
+    int max = values[0];
+    
+    for (int i = 1; i < values.length; i++)  
+    {
+        if (values[i] > max) 
+        {
+            max = values[i];
+        }
+        if (values[i] < min) 
+        {
+            min = values[i];
+        }
+    }
+    
+    data inventoryArray = new data();
+    inventoryArray.setLargest(max);
+    inventoryArray.setSmallest(min);
+    
+    double[][] fullInventory = {
+        {inventoryValueOne, inventoryValueTwo, inventoryValueThree}, 
+        {data.inventoryOne, data.inventoryTwo, data.inventoryThree}, 
+  };
+    
+
     System.out.println("The total value of all inventory is $" + totalValue);
     System.out.println("The average value of inventory is $" + averageValue);
     System.out.println("The most valueable product inventory is $" + mostValueable);
@@ -50,56 +86,63 @@ public class IntegrationProject {
     System.out.println(intCasting);
     System.out.println(doubleCasting);
     System.out.println(inventoryValueOne == inventoryValueThree);
+    System.out.println("The largest inventory is " + inventoryArray.getLargest());
+    System.out.println("The smallest inventory is " + inventoryArray.getSmallest());
+    System.out.println("There are " + fullInventory[1][0] + " items in inventory for item one, with a value of " + fullInventory[0][0]);
+    System.out.println("There are " + fullInventory[1][1] + " items in inventory for item two, with a value of " + fullInventory[0][1]);
+    System.out.println("There are " + fullInventory[1][2] + " items in inventory for item three, with a value of " + fullInventory[0][2]);
+    
+    for (double count : values) {
+      inventorySum += count;
+      System.out.println(inventorySum);
+    }
     return totalValue;
+    
+   
   }
 
   public static void calculate() { // this is a header
 
+      
     try (Scanner scan = new Scanner(System.in)) {
-      int inventoryOne = 25; // # of inventory of itemOne
-      int inventoryTwo = 20;  
-      int inventoryThree = 15;
+      
       int inventory = 0;
       final double COST_ONE = 24.05; // putting final in front of a variable means it cannot be
       final double COST_TWO = 15.92;
       final double COST_THREE = 28.67;
       double cost = 0;
-      double inventoryValueOne = inventoryOne * COST_ONE;
-      double inventoryValueTwo = inventoryTwo * COST_TWO;
-      double inventoryValueThree = inventoryThree * COST_THREE;
+      double inventoryValueOne = data.inventoryOne * COST_ONE;
+      double inventoryValueTwo = data.inventoryTwo * COST_TWO;
+      double inventoryValueThree = data.inventoryThree * COST_THREE;
       double inventoryValue = 0;
       String prompt = "Are you adding (true) or subtracting (false) inventory?";
-      int item;
+      int item = scan.nextInt();;
 
-      item = scan.nextInt();
 
       switch (item) {
 
         case 1:
           cost = COST_ONE;
           inventoryValue = inventoryValueOne;
-          inventory = inventoryOne;
-
+          inventory = data.inventoryOne;
           inventoryValueOne = inventoryValue;
-          inventoryOne = inventory;
+          data.inventoryOne = inventory;
           break;
 
         case 2:
           cost = COST_TWO;
           inventoryValue = inventoryValueTwo;
-          inventory = inventoryTwo;
-
+          inventory = data.inventoryTwo;
           inventoryValueTwo = inventoryValue;
-          inventoryTwo = inventory;
+          data.inventoryTwo = inventory;
           break;
 
         case 3:
           cost = COST_THREE;
           inventoryValue = inventoryValueThree;
-          inventory = inventoryThree;
-
+          inventory = data.inventoryThree;
           inventoryValueThree = inventoryValue;
-          inventoryThree = inventory;
+          data.inventoryThree = inventory;
           break;
       }
 
@@ -135,20 +178,22 @@ public class IntegrationProject {
       System.out.println(inventory);
       System.out.print("The current inventory value of item " + item + " is $");
       System.out.println(inventoryValue);
-      
+
       total(inventoryValueOne, inventoryValueTwo, inventoryValueThree); // this is a method call
-                                                              // they are arguments inside ()
+      // they are arguments inside ()
+
     }
   }
 
+  
 
-  public static void main(String[] args) { 
-
-
+  public static void main(String[] args){
+   
     Random rand = new Random();
 
     int compared = 0; // == only shows as true when it is pointing to the same
     Scanner scan = new Scanner(System.in); // object
+
     System.out.println("Would You like to access this tool?");
     String access;
     do {
@@ -165,7 +210,7 @@ public class IntegrationProject {
         System.out.println("Welcome to \"CTG LLC\"."); // escape sequence
         System.out.println("This promgram will keep track of inventory for the following sites");
 
-        for (String siteList : websites) { //for loop
+        for (String siteList : websites) { // for loop
           System.out.println(siteList);
 
         }
@@ -219,10 +264,12 @@ public class IntegrationProject {
 
     while (compared == -3 || compared == 0);
     System.out.println("Would you like to run this tool again?");
+    scan.hasNextLine();
     access = scan.nextLine();
 
     scan.close();
   }
+
 
 }
 
---
+++ Integration Project/src/data.java
@@ -0,0 +1,26 @@
+
+public class data {
+
+  public static int inventoryOne = 25; // # of inventory of itemOne
+  public static int inventoryTwo = 20;
+  public static int inventoryThree = 15;
+  
+  private int smallest;
+  private int largest;
+  
+    public void setSmallest(int smallest) {
+      this.smallest = smallest;
+    }
+    public int getSmallest() {
+      return smallest;
+    }
+    public void setLargest(int largest) {
+      this.largest = largest;
+    }
+    public int getLargest() {
+      return largest;
+    }
+   }
+
+  
+  
